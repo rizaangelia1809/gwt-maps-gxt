fe743b0562b0c77d51d619bf0c9a63e1ae3a19ae
claudiushauptmann.com <claudiushauptmann.com@267d0464-a682-11dd-8184-3158da6531e3>
2008-11-04 17:10:55 +0000

    Switched from Inheritance to Composition. OverlayTip is now attached to the Marker via MarkerTipWrapper.

--- gwt-maps-gxt/src/com/claudiushauptmann/gwt/maps/gxt/client/OverlayTip.java
+++ gwt-maps-gxt/src/com/claudiushauptmann/gwt/maps/gxt/client/OverlayTip.java
@@ -16,9 +16,6 @@
 package com.claudiushauptmann.gwt.maps.gxt.client;
 
 import com.extjs.gxt.ui.client.widget.tips.Tip;
-import com.google.gwt.maps.client.MapWidget;
-import com.google.gwt.maps.client.geom.LatLng;
-import com.google.gwt.maps.client.geom.Point;
 
 public class OverlayTip extends Tip  {
 	private String title;
@@ -46,35 +43,5 @@ public class OverlayTip extends Tip  {
 	      if (description != null) {
 	        getBody().update(description);
 	      }
-	}
-
-	
-	/*
-	 * Utility method since mapWidget.convertLatLngToDivPixel(latlng)
-	 * seems not to work correctly at the moment
-	 */
-	protected Point LatLng2Point(MapWidget mapWidget, LatLng ll) {
-		double n = mapWidget.getBounds().getNorthEast().getLatitude();
-		double s = mapWidget.getBounds().getSouthWest().getLatitude();
-		double e = mapWidget.getBounds().getNorthEast().getLongitude();
-		double w = mapWidget.getBounds().getSouthWest().getLongitude();
-		double lat = ll.getLatitude();
-		double lng = ll.getLongitude();
-		int height = mapWidget.getSize().getHeight();
-		int width = mapWidget.getSize().getWidth();
-		int offy = mapWidget.getAbsoluteTop();
-		int offx = mapWidget.getAbsoluteLeft();
-		
-		long fx = Math.round((lng-w)/(e-w)*width);
-		String sx = String.valueOf(fx);
-		int x = Integer.valueOf(sx);
-		x += offx;
-	
-		long fy = Math.round((lat-n)/(s-n)*height);
-		String sy = String.valueOf(fy);
-		int y = Integer.valueOf(sy);
-		y += offy;
-			
-		return Point.newInstance(x, y);
-	}
+	}	
 }
